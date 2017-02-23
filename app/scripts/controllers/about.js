4aa2e215965e1a296cf9f720ebc8767ad2104341
Hang Tu Wong Ley Franco <hwongley@lomedic.local>
2017-02-23 12:54:50 -0600

    Sunblue Page

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sunBlueApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sunBlueApp
+ */
+angular.module('sunBlueApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sunBlueApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sunBlueApp
+ */
+angular.module('sunBlueApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
